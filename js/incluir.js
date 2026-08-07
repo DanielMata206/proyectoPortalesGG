bb338438a0d200932d271e027ed78f00b801ce79
joseph03101 <jj03martinezb@gmail.com>
2026-08-06 20:52:38 -0600

    logo todos

--- js/incluir.js
+++ js/incluir.js
@@ -1,3 +1,11 @@
+if (!document.querySelector('link[rel="icon"]')) {
+    const favicon = document.createElement('link');
+    favicon.rel = 'icon';
+    favicon.type = 'image/png';
+    favicon.href = 'img/logo-panah.png';
+    document.head.appendChild(favicon);
+}
+
 fetch('header-footer/header.html')
     .then(res => res.text())
     .then(data => {
